636ac8c3ee1f292d8440d78cce5ca49dcba75faf
Abdellah BENBOUCHTA <abbenbouchta@gmail.com>
2017-01-25 08:15:55 +0000

    version 2
    version 2

--- models/partner.py
+++ models/partner.py
@@ -11,11 +11,27 @@ from odoo.exceptions import UserError,ValidationError
 class Partner(models.Model):
     _inherit = 'res.partner'
 
+    old_code = fields.Char(string="Code client", required=False)
+    code = fields.Char(string="Numéro client", required=False)
+
+    @api.multi
+    @api.depends('name', 'code')
+    def name_get(self):
+        result = []
+        for partner in self:
+            name=partner.name or ''
+            if partner.code:
+                name = '['+partner.code+']'+name
+            result.append((partner.id, name))
+        return result
+
     @api.model
     def create(self, data):
         user = self.env['res.users'].browse(self.env.uid)
         if not user.has_group('partner_wkf.partner_group_user'):
              raise UserError(_("Vous n'avez pas le droit de créer un partenaire"))
+        if data['customer']:
+            data['code']=self.env['ir.sequence'].next_by_code('customer.sequence') or _('New')
         partner = super(Partner, self.with_context(mail_create_nolog=True)).create(data)
         partner.message_post(body=_('%s a crée le partenaire %s') % (user.name, partner.name))
         return partner
